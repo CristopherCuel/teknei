d942db17f8902f80e7a0b6387cd90be509bd4f3a
Cristopher Aldo Cuel Monroy <cristopher.cuel@kode.com.mx>
2024-10-01 23:20:52 -0600

    first commit

---
+++ teknei/src/main/java/com/teknei/TekneiApplication.java
@@ -0,0 +1,13 @@
+package com.teknei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekneiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekneiApplication.class, args);
+	}
+
+}
